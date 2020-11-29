78ae1eae27f027ac2e5575f6fff2c86f7beb5b35
Hamidreza Fazli <hr.fazli@gmail.com>
2020-11-29 23:22:44 +0330

    make Tag model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function setSlugAttribute()
+    {
+        $this->attributes['slug'] = Str::slug(strtoupper($this->attributes['title']));
+    }
+
+    public function posts()
+    {
+        $this->belongsToMany(Post::class);
+    }
+
 }
