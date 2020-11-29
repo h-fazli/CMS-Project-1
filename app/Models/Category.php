50cbb1e1bbe592d81cd6dce0a4b78fc1043e4672
Hamidreza Fazli <hr.fazli@gmail.com>
2020-11-29 23:31:16 +0330

    make Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function setSlugAttribute()
+    {
+        $this->attributes['slug'] = Str::slug(strtoupper($this->attributes['title']));
+    }
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+
+    public function images()
+    {
+        return $this->morphOne(Image::class,'imageable');
+    }
 }
